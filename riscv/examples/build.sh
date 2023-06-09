fe6c298eb0fcbaf99487f8a303b2ee8960ccf569
Your Name <you@example.com>
2023-06-08 18:46:56 -0700

    build script

--- riscv/examples/build.sh
+++ riscv/examples/build.sh
@@ -1,4 +1,12 @@
 export CC=riscv64-linux-gnu-gcc
 export LD=riscv64-linux-gnu-ld
+export OD=riscv64-linux-gnu-objdump
+
+export CFLAGS="-g"
 #export CFLAGS="-march=rv32i -mabi=ilp32"
-make
+
+for prog in hello_world
+do
+  make $prog
+  $OD -S $prog > $prog.dis
+done
